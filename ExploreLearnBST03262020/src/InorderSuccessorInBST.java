85ec674455a601d6dd46545ee416590dfc56cd72
Elliiee <danyiluozhang@gmail.com>
2020-03-26 10:08:11 -0500

    285

--- ExploreLearnBST03262020/src/InorderSuccessorInBST.java
+++ ExploreLearnBST03262020/src/InorderSuccessorInBST.java
@@ -10,4 +10,20 @@ public class InorderSuccessorInBST {
             return (left != null) ? left : root;
         }
     }
+
+    public static void main(String[] args){
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(6);
+        root.left.left = new TreeNode(2);
+        root.left.right = new TreeNode(4);
+        root.left.left.left = new TreeNode(3);
+
+        InorderSuccessorInBST successor = new InorderSuccessorInBST();
+        TreeNode result = successor.successor(root, root.left);
+        if (result == null)
+            System.out.println("null");
+        else
+            System.out.println(result.val);
+    }
 }
