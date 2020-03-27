a4c2f66e43cc6164419d80fc2868be0236874655
Elliiee <danyiluozhang@gmail.com>
2020-03-27 15:32:52 -0500

    95

--- RecursionI03272020/src/UniqueBSTII.java
+++ RecursionI03272020/src/UniqueBSTII.java
@@ -3,34 +3,39 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class UniqueBSTII {
-    public LinkedList<TreeNode> generateTrees(int start, int end){
-        LinkedList<TreeNode> allTrees = new LinkedList<>();
-
+    public LinkedList<TreeNode> generate_trees(int start, int end) {
+        LinkedList<TreeNode> all_trees = new LinkedList<TreeNode>();
         if (start > end) {
-            allTrees.add(null);
-            return allTrees;
+            all_trees.add(null);
+            return all_trees;
         }
 
-        for (int i = start; i <= end; i++){
-            LinkedList<TreeNode> leftTrees = generateTrees(start, i - 1);
-            LinkedList<TreeNode> rightTrees = generateTrees(i + 1, end);
+        // pick up a root
+        for (int i = start; i <= end; i++) {
+            // all possible left subtrees if i is choosen to be a root
+            LinkedList<TreeNode> left_trees = generate_trees(start, i - 1);
 
-            for (TreeNode l : leftTrees){
-                for (TreeNode r : rightTrees){
-                    TreeNode currentTree = new TreeNode();
-                    currentTree.left = l;
-                    currentTree.right = r;
-                    allTrees.add(currentTree);
+            // all possible right subtrees if i is choosen to be a root
+            LinkedList<TreeNode> right_trees = generate_trees(i + 1, end);
+
+            // connect left and right trees to the root i
+            for (TreeNode l : left_trees) {
+                for (TreeNode r : right_trees) {
+                    TreeNode current_tree = new TreeNode(i);
+                    current_tree.left = l;
+                    current_tree.right = r;
+                    all_trees.add(current_tree);
                 }
             }
         }
-        return allTrees;
+        return all_trees;
     }
 
-    public List<TreeNode> generateTrees(int n){
-        if (n == 0) return new LinkedList<TreeNode>();
-
-        return generateTrees(1, n);
+    public List<TreeNode> generateTrees(int n) {
+        if (n == 0) {
+            return new LinkedList<TreeNode>();
+        }
+        return generate_trees(1, n);
     }
 }
 /*
