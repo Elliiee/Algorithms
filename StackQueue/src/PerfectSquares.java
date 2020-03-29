f49bc96b71e5d1d72de05bfca0c583c5872b3a8f
Elliiee <danyiluozhang@gmail.com>
2020-03-29 11:23:54 -0500

    20

---
+++ StackQueue/src/PerfectSquares.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+public class PerfectSquares {
+    public int numSquares(int n){
+        ArrayList<Integer> square_nums = new ArrayList<>();
+        for (int i = 1; i * i <= n; i++){
+            square_nums.add(i * i);
+        }
+
+        Set<Integer> queue = new HashSet<>();
+        queue.add(n);
+
+        int level = 0;
+        while (!queue.isEmpty()){
+            level++;
+            Set<Integer> next_queue = new HashSet<>();
+
+            for (Integer remainder : queue){
+                for (Integer square : square_nums){
+                    if (remainder.equals(square))
+                        return level;
+                    else if (remainder < square)
+                        break;
+                    else
+                        next_queue.add(remainder - square);
+                }
+            }
+            queue = next_queue;
+        }
+        return level;
+    }
+}
